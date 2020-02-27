b2c02a8e347d9994eb98a6307baccb28709dde1f
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-27 11:17:55 +0200

    Add files via upload

---
+++ JavaCore 1102 Pets/src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+/*
+
+1101 Horse and Pegasus
+Edit two classes: Horse (horse) and Pegas (Pegasus).
+Inherit Pegasus from a horse.
+
+Requirements:
+1. The Solution class must have a Horse class.
+2. The Horse class must be public.
+3. The Solution class must have the Pegas class.
+4. The Pegas class must be public.
+5. The Pegas class must be inherited from the Horse class.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {}
+    public class Pet{
+        private String name;
+        private int age;
+        private int speed;
+    }
+    public class Dog extends Pet{
+        private String a;
+        private String b;
+    }
+    public class Cat extends Pet{
+        private String a;
+        private String b;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
